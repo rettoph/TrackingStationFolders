77992abe99e3e01e9622d2024528b2680a0c14e8
rettoph <anthony@turner.ec>
2023-11-13 00:54:43 -0700

    Initial commit

---
+++ TrackingStationFolders/TrackingStationFolders.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TrackingStationFolders
+{
+    [KSPAddon(KSPAddon.Startup.TrackingStation, false)]
+    public class TrackingStationFolders : MonoBehaviour
+    {
+        public TrackingStationFolders()
+        {
+
+        }
+
+        public void Awake()
+        {
+            Console.WriteLine("Test123");
+            var test = UnityEngine.Object.FindObjectOfType<GameObject>();
+        }
+    }
+}
